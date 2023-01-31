1e16323b00228b1161fdf25000f71a0edcda4e11
Wojciech Ciezobka <wciezobka@student.agh.edu.pl>
2023-01-31 16:42:23 +0100

    Add scripts for power static scenarios (not tested)

---
+++ tools/slurm/power_static/ml.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/scl enable devtoolset-11 rh-python38 -- /bin/bash -l
+
+ML4WIFI_DIR="${ML4WIFI_DIR:=$HOME/ftmrate_internal/ml4wifi}"
+TOOLS_DIR="${TOOLS_DIR:=$HOME/ftmrate_internal/tools}"
+
+cd "$ML4WIFI_DIR/envs"
+
+SEED_SHIFT=$1
+MANAGER=$7
+MANAGER_NAME=$8
+N_WIFI=$4
+DISTANCE=$2
+DELTA=$3
+INTERVAL=$5
+SIM_TIME=$6
+MEMPOOL_SHIFT=$9
+
+SEED=$(( SEED_SHIFT + SLURM_ARRAY_TASK_ID ))
+MEMPOOL_KEY=$(( MEMPOOL_SHIFT + SLURM_ARRAY_TASK_ID ))
+
+CSV_PATH="$VAR_POWER_DIR/data/${MANAGER_NAME}_adopt_nwifi${N_WIFI}_dist${DISTANCE}_delta${DELTA}_interval${INTERVAL}_seed${SEED}"
+
+WARMUP_TIME=$(( N_WIFI + 4))
+FUZZ_TIME=$(( N_WIFI / 2 + 2 ))
+LOSS_MODEL="Nakagami"
+
+python3 ml_wifi_manager.py --scenario="distance" --ml_manager="$MANAGER" --managerName="$MANAGER_NAME" --distance="$DISTANCE" --nWifi="$N_WIFI" --simulationTime="$SIM_TIME" --warmupTime="$WARMUP_TIME" --fuzzTime="$FUZZ_TIME" --lossModel="$LOSS_MODEL" --seed="$SEED" --interval="$INTERVAL" --delta="$DELTA" --adaptPower --csvPath="$CSV_PATH" --mempoolKey="$MEMPOOL_KEY"
--- tools/slurm/run-ml-scenarios.sh
+++ tools/slurm/run-ml-scenarios.sh
@@ -13,6 +13,8 @@ SHIFT=0
 SEED_SHIFT=100
 BASE_MEMPOOL=3000
 
+### Basic scenarios
+
 run_equal_distance() {
   N_REP=10
   N_POINTS=13
@@ -83,6 +85,40 @@ run_moving() {
   done
 }
 
+### var-power scenarios
+
+run_power_static() {
+  N_WIFI=$1
+  DELTA=$2
+  N_REP=10
+  DISTANCE=7
+
+  INTERVALS=("0.00215" "0.00464" "0.01" "0.02154" "0.04642" "0.1" "0.21544" "0.46416" "1.0" "2.15443" "4.64159" "10.0")
+  SIM_TIMES=("10.0" "10.0" "10.1" "10.3" "10.5" "11.0" "12.2" "14.7" "20" "31.6" "56.5" "110")
+  N_POINTS=${#INTERVALS[@]}   # mean_interval = a * q^n, a=0.001, q=2.15443469, n=1..12
+
+  for (( i = 0; i < MANAGERS_LEN; i++ )); do
+    MANAGER=${MANAGERS[$i]}
+    MANAGER_NAME=${MANAGERS_NAMES[$i]}
+    ARRAY_SHIFT=0
+
+    for (( j = 0; j < N_POINTS; j++)); do
+      INTERVAL=${INTERVALS[$j]}
+      SIM_TIME=${SIM_TIMES[$j]}
+
+      START=$ARRAY_SHIFT
+      END=$(( ARRAY_SHIFT + N_REP - 1 ))
+
+      MEMPOOL_SHIFT=$(( SHIFT + BASE_MEMPOOL ))
+      ARRAY_SHIFT=$(( ARRAY_SHIFT + N_REP ))
+
+      sbatch -p gpu --array=$START-$END "$TOOLS_DIR/slurm/power_static/ml.sh" "$SEED_SHIFT" "$MANAGER" "$MANAGER_NAME" "$N_WIFI" "$DISTANCE" "$DELTA" "$INTERVAL" "$SIM_TIME" "$MEMPOOL_SHIFT"
+    done
+
+    SHIFT=$(( SHIFT + N_POINTS * N_REP ))
+  done
+}
+
 run_power_moving() {
   N_REP=15
   DELTA=$1
@@ -103,23 +139,25 @@ run_power_moving() {
   done
 }
 
-echo -e "\nQueue equal distance (d=0) scenario"
-run_equal_distance 0
+### Run section
 
-echo -e "\nQueue equal distance (d=20) scenario"
-run_equal_distance 20
+# echo -e "\nQueue equal distance (d=0) scenario"
+# run_equal_distance 0
 
-echo -e "\nQueue moving station (v=1) scenario"
-run_moving 1 56 1
+# echo -e "\nQueue equal distance (d=20) scenario"
+# run_equal_distance 20
 
-echo -e "\nQueue moving station (v=2) scenario"
-run_moving 2 28 "0.5"
+# echo -e "\nQueue moving station (v=1) scenario"
+# run_moving 1 56 1
 
-echo -e "\nQueue static stations scenario"
-run_rwpm 0
+# echo -e "\nQueue moving station (v=2) scenario"
+# run_moving 2 28 "0.5"
 
-echo -e "\nQueue mobile stations scenario"
-run_rwpm "1.4"
+# echo -e "\nQueue static stations scenario"
+# run_rwpm 0
+
+# echo -e "\nQueue mobile stations scenario"
+# run_rwpm "1.4"
 
 echo -e "\nQueue power with moving station (delta=5, interval=5) scenario"
 run_power_moving 5 5
@@ -132,3 +170,9 @@ run_power_moving 5 1
 
 echo -e "\nQueue power with moving station (delta=15, interval=1) scenario"
 run_power_moving 15 1
+
+echo -e "\nQueue power with single static station (nWiFi=1, delta=15) scenario"
+run_power_static 1 15
+
+echo -e "\nQueue power with multiple static stations (nWiFi=10, delta=15) scenario"
+run_power_static 10 15
