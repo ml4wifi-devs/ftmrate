dc179d53ae7de28c3a70d4dbd3715e59c4b23be3
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-06-13 16:02:47 -0400

    Improve KL div calculation

--- ml4wifi/params_fit/__init__.py
+++ ml4wifi/params_fit/__init__.py
@@ -11,4 +11,4 @@ ES_BETA = 0.14975710213184357
 
 # Kalman filter distance sensor noise - calculated as the variance of normal distribution which is
 # most similar to distance_noise distribution in terms of KL divergence
-KF_SENSOR_NOISE = 0.7455304265022278
+KF_SENSOR_NOISE = 0.7454228401184082
