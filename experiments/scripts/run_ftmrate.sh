cec5a8656b1867618222301e064c0cc040974a1d
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-08-17 12:50:31 +0200

    Fix FTMRate venv

--- experiments/scripts/run_ftmrate.sh
+++ experiments/scripts/run_ftmrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source $1/frames_venv/bin/activate
-sudo $1/frames_venv/bin/python3.8 -u $1/ftmrate_internal/experiments/ftmrate.py
+source $1/ftmrate_venv/bin/activate
+sudo $1/ftmrate_venv/bin/python3.8 -u $1/ftmrate_internal/experiments/ftmrate.py
