04844649417c776c3273c0b22247b10c21bea7ec
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2023-10-14 21:28:58 +0200

    Final configuration of plots

--- tools/slurm/generate-plots.sh
+++ tools/slurm/generate-plots.sh
@@ -6,10 +6,10 @@ export PYTHONPATH="$PYTHONPATH:$TOOLS_DIR"
 bash $TOOLS_DIR/outputs/combine_csv_files.sh
 python3 $TOOLS_DIR/plots/data_rates_plots.py
 python3 $TOOLS_DIR/plots/equal-distance_overhead.py
-python3 $TOOLS_DIR/plots/equal-distance_t-tests.py
 python3 $TOOLS_DIR/plots/rwpm_plots.py
 python3 $TOOLS_DIR/plots/rwpm_t-tests.py
 # python3 $TOOLS_DIR/plots/equal-distance_plots.py
+# python3 $TOOLS_DIR/plots/equal-distance_t-tests.py
 # python3 $TOOLS_DIR/plots/hidden-node-distance_plots.py
 # python3 $TOOLS_DIR/plots/hidden-node-nwifi_plots.py
 # python3 $TOOLS_DIR/plots/hidden-node_violin-plots.py
